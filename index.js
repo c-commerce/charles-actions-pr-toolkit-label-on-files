3c2a4b1362ed2d440416ebec2a86ab91215caaad
Robert Jefe Lindstädt <robert.lindstaedt@gmail.com>
2022-06-30 03:10:05 +0000

    feat: label pr mvp

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
-const core = require('@actions/core');
-const wait = require('./wait');
-
+const core = require('@actions/core')
+const github = require('@actions/github')
 
 // most @actions toolkit packages have async methods
-async function run() {
-  try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
-
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
-
-    core.setOutput('time', new Date().toTimeString());
-  } catch (error) {
-    core.setFailed(error.message);
+async function run () {
+  if (!github.context.payload.pull_request) {
+    core.info('skipping as this context does not seem to be a PR')
+    return core.setOutput('status', 'skipped')
   }
+
+  await github.getOctokit().rest.issues.addLabels({
+    repo: github.context.repo.repo,
+    owner: github.context.repo.owner,
+    issue_number: github.context.payload.pull_request.number,
+    labels: ['hello-world']
+  })
 }
 
-run();
+run()
